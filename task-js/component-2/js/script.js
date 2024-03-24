38dfa3fc391730ac7ce5ce62af301e242a0f640c
maxtm13 <maxtm13@gmail.com>
2024-03-24 23:33:24 +0300

    second js-test begin

--- task-js/component-2/js/script.js
+++ task-js/component-2/js/script.js
@@ -1,5 +1,13 @@
 (function() {
   /* Код компонента пишите здесь */
 
+const phone = document.getElementById('phone')
+const trueNumber = '0123456789() -'.split('');
+console.log('trueNumber:' + trueNumber);
 
+console.log('phone:' + phone);
+
+phone.addEventListener('keydown',()=>{
+  console.log('phone:' + phone.value);
+})
 })();
