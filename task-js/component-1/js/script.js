0575f12ed69eab942980291ab475560ca5747505
maxtm13 <maxtm13@gmail.com>
2024-03-24 22:53:21 +0300

    first js-test

---
+++ task-js/component-1/js/script.js
@@ -0,0 +1,60 @@
+(function () {
+  /**
+   * Служебная функция для считывания параметров из адресной строки
+   * и определения конфигурации компонента
+   * @param  {string} name - имя параметра
+   * @return {number} - значение параметра в адресной строке
+   */
+  const getUrlValue = (name) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    return parseInt(urlParams.get(name), 10);
+  };
+
+  /**
+   * Настройки аккордеона, параметры получаются из командной строки
+   *
+   * tabs_limit - number, максимальное количество одновременно открытых элементов,
+   * по умолчанию 0 - не ограничено
+   *
+   * Для тестирования работы своего скрипта при разных значениях tabs_limit
+   * временно переопределяйте значение переменной, хранящей этот параметр.
+   * Либо можете дописыват GET-параметр с нужным значением в конец адресной строки,
+   * например: ?tabs_limit=1
+   */
+  const settings = {
+    tabsLimit: getUrlValue('tabs_limit') || 0,
+  };
+
+  /* Код компонента пишите ниже */
+
+  const tabs = Array.from(document.getElementsByClassName('accordeon-item-title'));
+
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', tabEvent);
+  });
+
+  let activeTab = [];
+
+  function tabEvent(e) {
+    if (settings.tabsLimit > 0) {
+      addActiveTab(e.target);
+    }
+  }
+
+  
+  function addActiveTab(item) {
+    let indexIfPresent = activeTab.indexOf(item);
+    if (indexIfPresent === -1) {
+      if (activeTab.length >= settings.tabsLimit) {
+        let removeTab = activeTab.shift();
+        removeTab.closest('.accordeon-item').classList.remove('accordeon-item--open')
+      }
+      item.closest('.accordeon-item').classList.add('accordeon-item--open')
+      activeTab.push(item)
+    }
+    else {
+      activeTab.splice(indexIfPresent, 1);
+      item.closest('.accordeon-item').classList.remove('accordeon-item--open');
+    }
+  }
+})();
