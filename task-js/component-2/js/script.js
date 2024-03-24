d23d4ea66c73500616816a448b96d8ea33861cd0
maxtm13 <maxtm13@gmail.com>
2024-03-24 23:38:16 +0300

    second js-test phone-testing

--- task-js/component-2/js/script.js
+++ task-js/component-2/js/script.js
@@ -6,7 +6,7 @@ const trueNumber = '0123456789() -'.split('');
 console.log('trueNumber:' + trueNumber);
 
 console.log('phone:' + phone);
-
+phone.value='+7';
 phone.addEventListener('keydown',()=>{
   console.log('phone:' + phone.value);
 })
